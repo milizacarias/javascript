45fcd2eae70efec63d470517111e2f390c133625
Mili Zacarias <milizacarias21@gmail.com>
2022-12-29 18:28:40 -0300

    declare algunas variables y operaciones

---
+++ js/main.js
@@ -0,0 +1,8 @@
+let precioProducto;
+do {
+  precioProducto = parseFloat(
+    prompt("ingrese precio de producto, para terminar ingrese STOP")
+  );
+  if (precioProducto > 0) {
+  }
+} while (precioProducto !== "STOP");
