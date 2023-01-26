572540b57e108c8a46d625e095cc74d3ffd94136
Mili Zacarias <milizacarias21@gmail.com>
2023-01-26 17:06:29 -0300

    correccion re entrega 2

--- js/main.js
+++ js/main.js
@@ -1,42 +1,26 @@
-const ejercicios = [
-  {
-    nombre: "back squat",
-    tren: "inferior",
-    musculo: "cuadriceps",
-    rm: "100",
-  },
-
-  {
-    nombre: "deadlift",
-    tren: "inferior",
-    musculo: "femorales",
-    rm: "200",
-  },
-
-  {
-    nombre: "shoulder press",
-    tren: "superior",
-    musculo: "Hombros",
-    rm: "50",
-  },
-
-  {
-    nombre: "push jerk",
-    tren: "superior",
-    musculo: "hombros",
-    rm: "65",
-  },
-
-  {
-    nombre: "dominadas",
-    tren: "superior",
-    musculo: "dorsales",
-    rm: "10",
-  },
-];
-
-ejercicios.forEach((ejercicios) => {
-  if (ejercicios.rm < 100) {
-    console.log(ejercicios.nombre);
+class Producto {
+  constructor(entrada) {
+    this.precio = parseFloat(entrada.precio);
+    this.cantidad = parseInt(entrada.cantidad);
   }
-});
+  calcularPrecioConIva() {
+    return this.cantidad * this.precio * 1.21;
+  }
+}
+
+let precio;
+let cantidad; 
+const carritoDeCompras = [];
+do {
+  precio = prompt("ingrese el precio del producto. Para finalizar ingrese FIN");
+  IF (precio !== "FIN") {
+    cantidad = prompt("ingrese la cantidad a comprar");
+    const producto = new Producto({ precio: precio, cantidad: cantidad});
+  }
+} while(precio !== "FIN");
+
+let total = 0;
+for(const producto of carritoDeCompras) {
+  total = total + producto.calcularPrecioConIva();
+}
+alert("el total del carrito es: " + total);
